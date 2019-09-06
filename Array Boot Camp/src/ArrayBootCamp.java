0a601b5fdbe2b81c837e965d55eff6f0ddca11c4
nicholas.giacomozzi <nicholas.giacomozzi@mu-c1-03.mullen.pvt.k12.co.us>
2019-09-06 12:06:50 -0600

    Finish Last Project

--- Array Boot Camp/src/ArrayBootCamp.java
+++ Array Boot Camp/src/ArrayBootCamp.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class ArrayBootCamp
 	{
+		static int number;
 
 		public static void main(String[] args)
 			{
-				printSweets();
-				insertEightNumbers();
+				// printSweets();
+				// insertEightNumbers();
 				generateRandomNumber();
 			}
 
@@ -26,20 +27,50 @@ public class ArrayBootCamp
 
 						favoriteSweets[i] = sweet;
 
-						System.out.println(favoriteSweets[i]);
-
+					}
+				for (String f : favoriteSweets)
+					{
+						System.out.println("Your favorite sweets are: " + f);
 					}
 
 			}
 
 		private static void insertEightNumbers()
 			{
+				Scanner intInput = new Scanner(System.in);
+
+				int[] numberList = new int[8];
+
+				int sumEven = 0;
+				int sumOdd = 0;
+
+				for (int i = 0; i < 8; i++)
+					{
+						System.out.println("Enter your number");
+
+						number = intInput.nextInt();
+
+						numberList[i] = number;
+
+						if (number % 2 == 0)
+							{
+								sumEven += numberList[i];
+							}
+						else
+							{
+								sumOdd += numberList[i];
+							}
+
+					}
+
+				System.out.println("The sum of all the even numbers is " + sumEven);
+				System.out.println("The sum of all the odd numbers is " + sumOdd);
 
 			}
 
 		private static void generateRandomNumber()
 			{
-
+				System.out.println("Please input five random two digit numbers.");
 			}
 
 	}
